2aeac0315a030036c485ef325b02236cd4ff8ad7
stryku <stryku2393@gmail.com>
2016-07-18 19:00:21 +0200

    hasToString update

--- Common/sources/inc/utils/traits/traits.hpp
+++ Common/sources/inc/utils/traits/traits.hpp
@@ -9,9 +9,10 @@ namespace utils
     {
         template<typename, typename T>
         struct hasToString {
-            static_assert(
+            /*static_assert(
                 std::integral_constant<T, false>::value,
-                "Second template parameter needs to be of function type.");
+                "Second template parameter needs to be of function type.");*/
+            static constexpr bool value = false;
         };
 
         template<typename C, typename Ret, typename... Args>
