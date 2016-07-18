82698a26b434d80764b63dd67636642bb5ce7fe9
stryku <stryku2393@gmail.com>
2016-07-18 19:03:29 +0200

    update

--- Common/sources/inc/utils/ToString.hpp
+++ Common/sources/inc/utils/ToString.hpp
@@ -6,24 +6,24 @@
 
 namespace utils
 {
-    //template <typename T, typename = typename std::enable_if_t<std::is_convertible<T, std::string>::value>>
-    template <typename T, typename = typename std::enable_if_t<std::is_convertible<T, std::string>::value>>
+    template <typename T, typename = std::enable_if_t<std::is_convertible<T, std::string>::value>>
     std::string toString(const T &value)
     {
         return{ value };
     }
 
-    template <class T, typename = void, typename = void, typename = std::enable_if_t<traits::hasToString<T, std::string()const>::value>>
-    std::string toString(const T &value)
-    {
-        return value.toString();
-    }
-
-    //template <class T, typename = typename std::enable_if_t<traits::CanCallToString<T>::value>>
-    template <class T, typename = void, typename = typename std::enable_if_t<traits::CanCallToString<T>::value>>
+    template <class T, typename = void, 
+              typename = std::enable_if_t<traits::CanCallToString<T>::value>>
     std::string toString(const T &value)
     {
         return std::to_string(value);
+    }
+
+    template <class T, typename = void, typename = void, 
+              typename = std::enable_if_t<traits::hasToString<T, std::string()const>::value>>
+    std::string toString(const T &value)
+    {
+        return value.toString();
     }
 
     std::string toString(bool value)
