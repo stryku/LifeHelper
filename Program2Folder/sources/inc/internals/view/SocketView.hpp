616f0f843a4e90e001cd2e0baf052ad5b86d9589
stryku <stryku2393@gmail.com>
2016-07-18 19:02:22 +0200

    update socket view

--- Program2Folder/sources/inc/internals/view/SocketView.hpp
+++ Program2Folder/sources/inc/internals/view/SocketView.hpp
@@ -2,7 +2,9 @@
 
 #include "View.h"
 
+#include <utils/message_builders/XmlMessageBuilder.hpp>
 #include <communication/CommunicationChannel.hpp>
+#include <utils/ToString.hpp>
 
 namespace P2
 {
@@ -10,18 +12,32 @@ namespace P2
     {
         class SocketView : public View
         {
-        private:
-            using Sender = Common::Communication::ChannelType::Sender;
+            using Sender = Common::Communication::SenderChannel;
 
         public:
-            SocketView(Sender &sender)
+            SocketView(Sender &sender) :
+                m_sender(sender)
+            {}
 
             virtual void updateSum(size_t newSum) override
             {
+                using Elem = utils::MessageBuild::Xml::Element<std::string>;
 
+                utils::MessageBuild::Xml::Builder builder;
+
+                const std::vector<Elem> elements
+                {
+                    Elem{"msg.type", "updateSum"},
+                    Elem{"msg.value", utils::toString(newSum)}
+                };
+
+                builder.addElements(elements);
+
+                m_sender.send(builder.build());
             }
 
-
+        private:
+            Sender &m_sender;
         };
     }
 }
