a7500fef5954372f507a136e3a5e1fdd69041f84
stryku <stryku2393@gmail.com>
2016-07-20 15:58:57 +0200

    improve toString

--- Common/sources/inc/utils/ToString.hpp
+++ Common/sources/inc/utils/ToString.hpp
@@ -6,22 +6,23 @@
 
 namespace utils
 {
-    template <typename T, typename = std::enable_if_t<std::is_convertible<T, std::string>::value>>
-    std::string toString(const T &value)
+    template <typename T>
+    std::enable_if<std::is_convertible<T, std::string>::value, std::string>::type
+        toString(const T &value)
     {
         return{ value };
     }
 
-    template <class T, typename = void, 
-              typename = std::enable_if_t<traits::CanCallToString<T>::value>>
-    std::string toString(const T &value)
+    template <typename T>
+    std::enable_if_t<traits::CanCallToString<T>::value, std::string>::type
+        toString(const T &value)
     {
         return std::to_string(value);
     }
 
-    template <class T, typename = void, typename = void, 
-              typename = std::enable_if_t<traits::hasToString<T, std::string()const>::value>>
-    std::string toString(const T &value)
+    template <typename T>
+    std::enable_if_t<traits::hasToString<T, std::string()const>::value, std::string>::type 
+        toString(const T &value)
     {
         return value.toString();
     }
