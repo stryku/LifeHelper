5cf76549be95afa62c44399b9cd160ccf828cd92
stryku <stryku2393@gmail.com>
2016-07-29 13:24:50 +0200

    added ProgramInfo formatting

---
+++ Program2Folder/sources/inc/internals/ProgramInfo.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace P2
+{
+    namespace Info
+    {
+        static constexpr auto programName = "Program2";
+        static constexpr auto uiFile = "Program2Form.ui";
+    }
+}
