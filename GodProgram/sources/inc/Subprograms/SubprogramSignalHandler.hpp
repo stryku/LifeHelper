2988ad4e6e0bb78dc976cdf018a4232d3785d72d
stryku <stryku2393@gmail.com>
2016-07-31 01:31:59 +0200

    added signals handling methods

--- GodProgram/sources/inc/Subprograms/SubprogramSignalHandler.hpp
+++ GodProgram/sources/inc/Subprograms/SubprogramSignalHandler.hpp
@@ -9,6 +9,13 @@ namespace God
 {
     namespace Subprograms
     {
+        enum class SignalType
+        {
+            HEARTBEAT,
+            ERROR,
+            CLEAN_CLOSE
+        };
+
         struct SignalsHandlers
         {
             std::function<void()> heartbeatLostCallback;
@@ -21,29 +28,63 @@ namespace God
         public:
             SignalsHandler(const SignalsHandlers &handlers,
                            const std::string &addressToConnect, 
-                           const std::string &subscriptionString) :
+                           const std::string &subscriptionString) noexcept :
                 heartbeatLostCallback{ handlers.heartbeatLostCallback },
                 errorCallback{ handlers.errorCallback },
                 cleanCloseCallback{ handlers.cleanCloseCallback },
                 heartbeatController{ heartbeatLostCallback }
             {}
 
-            void setHeartbeatLostCallback(std::function<void()> callback)
+            void setHeartbeatLostCallback(std::function<void()> callback) noexcept
             {
                 heartbeatLostCallback = callback;
             }
 
-            void setErrorCallback(std::function<void()> callback)
+            void setErrorCallback(std::function<void()> callback) noexcept
             {
                 errorCallback = callback;
             }
 
-            void setCleanCloseCallback(std::function<void()> callback)
+            void setCleanCloseCallback(std::function<void()> callback) noexcept
             {
                 cleanCloseCallback = callback;
             }
 
+            template <typename ...Args>
+            void signal(SignalType type, Args... args)
+            {
+                switch (type)
+                {
+                case God::Subprograms::SignalType::HEARTBEAT:
+                    handleHeartbeat();
+                    break;
+
+                case God::Subprograms::SignalType::ERROR:
+                    handleError(std::forward<Args>(args...));
+                    break;
+
+                case God::Subprograms::SignalType::CLEAN_CLOSE:
+                    handleCleanClose();
+                    break;
+
+                default:
+                    //todo log
+                    break;
+                }
+            }
+
         private:
+            void handleHeartbeat()
+            {
+                heartbeatController.beat();
+            }
+
+            void handleError(const std::string &errorMsg)
+            {}
+
+            void handleCleanClose()
+            {}
+
             std::function<void()> heartbeatLostCallback;
             std::function<void()> errorCallback;
             std::function<void()> cleanCloseCallback;
