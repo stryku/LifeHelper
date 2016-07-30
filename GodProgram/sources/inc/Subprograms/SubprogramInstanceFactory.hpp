6d704f2f03772732f706a2a190cdb0921008f27e
stryku <stryku2393@gmail.com>
2016-07-30 15:26:36 +0200

    removed commented code

--- GodProgram/sources/inc/Subprograms/SubprogramInstanceFactory.hpp
+++ GodProgram/sources/inc/Subprograms/SubprogramInstanceFactory.hpp
@@ -26,27 +26,7 @@ namespace God
                 create(QTabWidget &tabWidget, Subprograms::ModelId modelId)
             {
                 return{ tabWidget, modelId };
-
-                //Instance<ProgramInternals> instance;
-                //Controller controller;
-                //View view(&tabWidget);
-
-                //controller.registerView(&view);
-
-
             }
-
-            /*Instance<P2::ProgramInternals> createProgram2(QTabWidget &tabWidget, Subprograms::ModelId modelId)
-            {
-                P2::ProgramInternals internals;
-                P2::Controller::Controller controller;
-
-                P2::View::QtView qtView(&tabWidget);
-
-                controller.registerView()
-
-                internals.
-            }*/
         };
     }
 }
