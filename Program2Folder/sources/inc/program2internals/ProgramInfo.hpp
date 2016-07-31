a3ff171173710928b17827147aa6968a1b92f81a
stryku <stryku2393@gmail.com>
2016-07-31 03:19:49 +0200

    added TypesPack struct

--- Program2Folder/sources/inc/program2internals/ProgramInfo.hpp
+++ Program2Folder/sources/inc/program2internals/ProgramInfo.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include "view/View.h"
+#include "controller/Controller.h"
+#include "input/Input.h"
+
 namespace P2
 {
     namespace Info
     {
+        struct TypesPack
+        {
+            using View = View::View;
+            using Controller = Controller::Controller;
+            using InputPropagator = Input::InputPropagator;
+        };
+
         static constexpr auto programName = "Program2";
         static constexpr auto uiFile = "Program2Form.ui";
     }
