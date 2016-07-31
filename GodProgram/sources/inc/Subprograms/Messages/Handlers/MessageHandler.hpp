a3c3fefc5f6767f87815d02b9756741e5e571940
stryku <stryku2393@gmail.com>
2016-07-31 03:00:00 +0200

    added handling Program signals in MessageHandler::handle

--- GodProgram/sources/inc/Subprograms/Messages/Handlers/MessageHandler.hpp
+++ GodProgram/sources/inc/Subprograms/Messages/Handlers/MessageHandler.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Subprograms/SubprogramSignalHandler.hpp>
+
 namespace God
 {
     namespace Subprograms
@@ -13,10 +15,27 @@ namespace God
                 class MessageHandler
                 {
                 public:
+                    MessageHandler(SignalsHandler &signalHandler) :
+                        signalHandler{ signalHandler }
+                    {}
+
                     void handle(const std::string &strMsg)
                     {
-                        static_cast<ConcreteHandler*>(this)->handle_impl(Parser::parse(strMsg));
+                        auto parsed = Parser::parse(strMsg);
+
+                        if (parsed.type == Messages::MessageType::MODEL_SIGNAL)
+                        {
+                            auto signalType = parsed.internalMessage.getType<SignalType>();
+                            signalHandler.signal(signalType);
+                        }
+                        else
+                        {
+                            static_cast<ConcreteHandler*>(this)->handle_impl(parsed.internalMessage);
+                        }
                     }
+
+                private:
+                    SignalsHandler &signalHandler;
                 };
             }
         }
