8cd98ecc7900496a54007c404ac1dffd0db8951d
stryku <stryku2393@gmail.com>
2016-07-31 03:36:32 +0200

    added proper attributes simplified template argument establishConnection

--- GodProgram/sources/inc/Subprograms/SubprogramInstance.hpp
+++ GodProgram/sources/inc/Subprograms/SubprogramInstance.hpp
@@ -1,49 +1,64 @@
 #pragma once
 
+#include <communication/OneWayChannel.hpp>
+
+#include <Subprograms/SubprogramSignalHandler.hpp>
+#include <Subprograms/SubprogramMessagesSubscriber.hpp>
+#include <Subprograms/Messages/Handlers/Program2MessageHandler.hpp>
+
+#include <program2internals/input/SocketInputObserverSender.hpp>
+
 #include <QTabWidget>
 
 namespace God
 {
     namespace Subprograms
     {
-        template <typename ProgramInternals,
-                  typename View,
-                  typename Controller,
-                  typename InputPropagator>
-            class Instance
+        template <typename TypesPack>
+        class Instance
         {
         public:
-            Instance(QTabWidget &tabWidget, ModelId modelId) :
+            Instance(QTabWidget &tabWidget, 
+                     ModelId modelId,
+                     const std::string &pushAddress,
+                     const std::string &subscribeAddress,
+                     const std::string &subscribeStr.
+                     SignalsHandler &signalHandler) :
                 internals { &controller },
                 view{ &tabWidget }, 
+                inputPropagator{ sender },
+                signalsHandler{ signalsHandler },
+                messageHandler{ signalsHandler },
+                messageSubscriber{ subscribeAddress, subscribeStr, messageHandler },
                 modelId{ modelId }
             {
                 internals.addView(&view);
                 internals.addInputPropagator(&inputPropagator);
-            }
 
-            void addView(View *view)
-            {
-                internals.addView(view);
-            }
-
-            void addInputPropagator(InputPropagator *input)
-            {
-                internals.addInputPropagator(input);
-            }
-
-            void setController(Controller *controller)
-            {
-                internals.setController(controller);
+                establishConnection(pushAddress, subscribeAddress);
+                messageSubscriber.startRecv();
             }
 
         private:
-            ProgramInternals internals;
-            View view;
-            Controller controller;
-            InputPropagator inputPropagator;
+            void establishConnection(const std::string &pushAddress,
+                                     const std::string &subscribeAddress)
+            {
+                sender.connect(pushAddress);
+                messageSubscriber.connect(subscribeAddress);
+            }
 
-            ModelId modelId;
+            TypesPack::ProgramInternals internals;
+            TypesPack::View view;
+            TypesPack::Controller controller;
+            TypesPack::SocketInputPropagator inputPropagator;
+
+            SignalsHandler &signalsHandler;
+            MessageHandler messageHandler;
+
+            Subscriber messageSubscriber;
+
+            Common::Communication::SenderChannel sender;
+            Common::Communication::SubscriberChannel subscriber;
         };
     }
 }
