618a005fb377ae92ba73a585f98c7deb9c1be8da
stryku <stryku2393@gmail.com>
2016-09-19 21:14:58 +0200

    introduce SocketView

--- Program2Folder/sources/inc/program2internals/view/SocketView.hpp
+++ Program2Folder/sources/inc/program2internals/view/SocketView.hpp
@@ -1,1 +1,46 @@
 #pragma once
+
+#include "Communication/OneWayChannel.hpp"
+#include "Communication/messages/XmlMessageBuilder.hpp"
+#include "utils/ToString.hpp"
+
+namespace P2
+{
+    namespace View
+    {
+        template <typename Sender>
+        class SocketView
+        {
+        public:
+            SocketView(Sender &&sender) :
+                sender{ std::move(sender) }
+            {}
+
+            SocketView(SocketView&&) = default;
+            SocketView& operator=(SocketView&&) = default;
+
+            SocketView(const SocketView&) = delete;
+            SocketView& operator=(const SocketView&) = delete;
+
+            void updateSum(size_t newSum)
+            {
+                using Elem = Common::Communication::MessageBuilders::Xml::Element<std::string>;
+
+                Common::Communication::MessageBuilders::Xml::Builder builder;
+
+                const std::vector<Elem> elements
+                {
+                    Elem{ "msg.type", "updateSum" },
+                    Elem{ "msg.internals", utils::toString(newSum) }
+                };
+
+                builder.addElements(elements);
+
+                sender.send(builder.build());
+            }
+
+        private:
+            Sender sender;
+        };
+    }
+}
