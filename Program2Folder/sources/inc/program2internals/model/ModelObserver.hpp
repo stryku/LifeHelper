f83cbe62180b264e7a65fdd79d9b5f9249c9caa1
stryku <stryku2393@gmail.com>
2016-09-21 22:14:14 +0200

    Made ModelObserver template with View as a parameter

---
+++ Program2Folder/sources/inc/program2internals/model/ModelObserver.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "utils/log.hpp"
+
+#include <memory>
+
+namespace P2
+{
+    namespace Model
+    {
+        template <typename View>
+        class ModelObserver
+        {
+        public:
+            ModelObserver(const ModelObserver&) = delete;
+            ModelObserver& operator=(const ModelObserver&) = delete;
+
+            void registerView(std::weak_ptr<View> view)
+            {
+                LOG_FILE("ModelObserver::registerView");
+
+                views.push_back(view);
+            }
+
+            void newSumValue(size_t newSum)
+            {
+                LOG_FILE("ModelObserver::newSumValue");
+
+                for (auto &weakView : views)
+                    if (auto view = weakView.lock())
+                        view->updateSum(newSum);
+            }
+
+        private:
+            std::vector<std::weak_ptr<View>> views;
+        };
+    }
+}
